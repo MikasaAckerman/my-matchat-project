ef7cebac3a375476a0cc2d9f1c8189fa6ebf61a9
LunarEclipse <949886@qq.com>
2025-06-09 17:33:59 +0800

    Hide iframe border.

--- src/views/TravelView.vue
+++ src/views/TravelView.vue
@@ -1,8 +1,6 @@
 <template>
     <div>
     <!-- 其他内容 -->
-    <iframe src="travel.html" style="width: 100%; height: 100vh;"></iframe>
+    <iframe src="travel.html" style="width: 100%; height: 100vh;" frameBorder="0"></iframe>
   </div>
 </template>
-  
-  
